1446856e1cda70a103beb166fbd53e2b4a938441
darrylayers <ayersda@appstate.edu>
2018-09-11 21:50:15 -0400

    Added test for ProgramTimer.java
    Began work on a test for ProgramTimer.java

---
+++ src/core/TestProgramTimer.java
@@ -0,0 +1,15 @@
+package core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestProgramTimer {
+
+    @Test
+    void testOneApp() {
+
+     //   assertEquals(expectedOutput, actualOutput);
+    }
+
+}
