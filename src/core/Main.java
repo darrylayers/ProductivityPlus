1f4d9d10bb45a853d32350c6633fff995a52da4d
darrylayers <ayersda@appstate.edu>
2018-09-11 21:32:26 -0400

    Added basic program timer functionality
    Program timer now works with two simple GUI buttons.

---
+++ src/core/Main.java
@@ -0,0 +1,128 @@
+package core;
+
+/*
+ * TODO: Need to find a way to stop people from hitting track multiple times.
+ *  more than one thread is being created and the times are being added.
+ *  
+ *  TODO: Add test cases
+ * 
+ */
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * Main class, currently holding all the GUI code.
+ * 
+ * @author Austin Ayers
+ * @version 9/11/18
+ */
+
+public class Main {
+
+    public static final int FRAME_SIZE = 300;
+    public JFrame frame;
+    public JLabel resultLabel;
+    public static JLabel resultLabel2;
+    public static JTextField leftOperand;
+    public static Main gui;
+    public static JLabel isTracking;
+
+    /**
+     * Main method that builds the GUI.
+     */
+    public static void main(String[] args) {
+        gui = new Main();
+    }
+
+    /**
+     * Constructor that builds the frame.
+     */
+    public Main() {
+        frame = new JFrame();
+        frame.setLocation(100, 100);
+        frame.setSize(FRAME_SIZE, FRAME_SIZE - 120);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("ProductivityPlus");
+        initializeComponents();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Method to initialize all the components onto the frame.
+     */
+    public void initializeComponents() {
+
+        // Create the buttons
+        JButton start = new JButton("Start");
+        start.setName("start");
+        start.addActionListener(new Start());
+        JButton stop = new JButton("Stop");
+        stop.setName("stop");
+        stop.addActionListener(new Stop());
+
+        // Create the button panel
+        JPanel positPanel = new JPanel();
+
+        // Add button to the panel
+        positPanel.add(start);
+        positPanel.add(stop);
+
+        // Add the panel to the bottom of the calculator window
+        frame.add(positPanel, BorderLayout.CENTER);
+        isTracking = new JLabel("Currently not tracking.");
+        JPanel labelPanel = new JPanel();
+        // Add the result label to the label panel, then frame.
+        labelPanel.add(isTracking);
+        frame.add(labelPanel, BorderLayout.PAGE_START);
+    }
+
+    /**
+     * Class that performs the start function.
+     */
+    public class Start implements ActionListener {
+
+        /**
+         * ActionEvent for addition.
+         * 
+         * @param e
+         *            takes the listener.
+         */
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            ProgramTimer programTimer = new ProgramTimer();
+            Thread t = new Thread(programTimer);
+            t.start();
+            isTracking.setText("Currently tracking...");
+
+        }
+    }
+
+    /**
+     * Class that performs the stop function.
+     */
+    public class Stop implements ActionListener {
+        /**
+         * ActionEvent for addition.
+         * 
+         * @param e
+         *            takes the listener.
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            ProgramTimer.stop();
+            isTracking.setText("Currently not tracking.");
+
+        }
+    }
+
+}
---
+++ src/core/ProgramTimer.java
@@ -0,0 +1,136 @@
+package core;
+
+import java.util.HashMap;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HWND;
+
+/**
+ * ProgramTimer class that performs the timing feature and records all the data.
+ * 
+ * @author Austin Ayers
+ * @version 9/11/18
+ */
+
+public class ProgramTimer implements Runnable {
+
+    private static HashMap<String, Long> appMap = new HashMap<>();
+    private static char[] buffer = new char[4096];
+    private static HWND hwnd;
+    private static String prog = "";
+    private static String progLast = "";
+    private static long newTime;
+    private static long oldTime;
+    private static long start;
+    private static long end;
+    protected static boolean trackIfTrue;
+    private static boolean leftApp = false;
+
+    /**
+     * Overridden run method to run the timer on a new thread. Tracks to see
+     * when the user swaps the program in focus and records the time
+     * differences. Operates on a pause set to 1/10 per second.
+     */
+    @Override
+    public void run() {
+        startTime();
+        trackIfTrue = true;
+        leftApp = false;
+
+        while (trackIfTrue) {
+
+            // Grab the current in-focus window
+            hwnd = User32.INSTANCE.GetForegroundWindow();
+            User32.INSTANCE.GetWindowText(hwnd, buffer, buffer.length);
+            prog = Native.toString(buffer);
+
+            // Check to see if current in focus is the same as previous.
+            if (!progLast.equals(prog) && !progLast.equals("")) {
+                leftApp = true;
+                endTime();
+                mapTime();
+                startTime();
+            }
+
+            progLast = prog;
+            // Pause for 1/10 second before next check
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        if (!leftApp) {
+            endTime();
+            mapTime();
+        }
+        printMap();
+    }
+
+    /*
+     *  Method to stop the infinite tracking while loop.
+     */
+    public static void stop() {
+        trackIfTrue = (false);
+    }
+
+    /*
+     *  Method to add the new program time count to the Hashmap.
+     */
+    public static void mapTime() {
+        // If the program has time existing, we need to add the new to the old
+        if (appMap.containsKey(progLast)) {
+            oldTime = appMap.get(progLast);
+            oldTime += newTime;
+            appMap.put(progLast, oldTime);
+        }
+        // else if the program has no time, simply put the key-value
+        else {
+            appMap.put(progLast, newTime);
+        }
+    }
+
+    /*
+     *  Method to start tracking the time a new window is in focus.
+     */
+    public static void startTime() {
+        start = System.nanoTime();
+    }
+
+    /*
+     *  Method to stop tracking when an old window is in focus. 
+     *  Must divide to convert to seconds from nanoseconds.
+     */
+    public static void endTime() {
+        end = System.nanoTime();
+        newTime = (end - start) / 1000000000;
+        // System.out.println("Closing session and counted " + newTime + "
+        // seconds.");
+    }
+
+    /*
+     *  Method to print the entire hashmap of program names and times.
+     */
+    public static void printMap() {
+        System.out.println("\n=====================");
+        for (String name : appMap.keySet()) {
+            String key = name.toString();
+            // String value = appMap.get(name).toString();
+            long value = appMap.get(name);
+
+            if (value > 60) {
+                System.out.print("Time spent in " + key + " was " + value / 60
+                    + " minutes and " + value % 60 + " seconds.");
+            }
+            else {
+                System.out.println(
+                    "Time spent in " + key + " was " + value + " seconds.");
+            }
+        }
+        System.out.println("=====================\n");
+    }
+
+}
