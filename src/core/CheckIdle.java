07ef8f00443b7967d0b647926d3005cd92074004
Ayers <ayersda@appd.appstate.edu>
2018-09-20 16:39:07 -0400

    improved formatting

--- src/core/CheckIdle.java
+++ src/core/CheckIdle.java
@@ -4,23 +4,21 @@ import java.awt.MouseInfo;
 import java.awt.Point;
 
 /**
- * CheckIdle.java is used to determine if the user is idle
- * and decides if the data being recorded should be recorded 
- * and used in the output.
+ * CheckIdle.java is used to determine if the user is idle and decides if the
+ * data being recorded should be recorded and used in the output.
  * 
  * @author Austin Ayers
  * @version 9/20/18
  *
  */
 public class CheckIdle implements Runnable {
-	
+
 	// TODO: What determines a user is idle?
 	// Lack of keystrokes and lack of mouse movement.
 	// Sometimes the user will be "active" but not moving
-	// their mouse or typing, so this option should be 
+	// their mouse or typing, so this option should be
 	// optional and have a variable/customizable threshold for the time.
-	
-	
+
 	public static Point old_p;
 	public static Point new_p;
 	static int old_x;
@@ -28,73 +26,69 @@ public class CheckIdle implements Runnable {
 	private static int new_x;
 	private static int new_y;
 	CheckIdle idle;
-	
-	
+
 	public CheckIdle() {
-        this.saveMouse();
-        Thread t = new Thread(this);
-        t.start();
+		this.saveMouse();
+		Thread t = new Thread(this);
+		t.start();
 
 	}
-	
+
 	/*
-	 * This method saves the (x,y) location of the mouse
-	 * on the screen.
+	 * This method saves the (x,y) location of the mouse on the screen.
 	 */
 	public void saveMouse() {
-		//Grab the current mouse point
+		// Grab the current mouse point
 		old_p = MouseInfo.getPointerInfo().getLocation();
 		old_x = old_p.x;
 		old_y = old_p.y;
 	}
-	
+
 	/*
-	 * This method checks for mouse movement for the given threshold
-	 * by comparing new coord values to old ones.
+	 * This method checks for mouse movement for the given threshold by comparing
+	 * new coord values to old ones.
 	 */
 	public boolean checkMouse() {
-		//Grab the current mouse point
+		// Grab the current mouse point
 		new_p = MouseInfo.getPointerInfo().getLocation();
 		new_x = new_p.x;
 		new_y = new_p.y;
-		
+
 		// this compares the old points to the new ones.
 		// only true when the mouse is in the same location.
 		if (old_x == new_x && old_y == new_y) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	/*
 	 * This method checks for keystrokes for the given threshold.
 	 */
 	public void checkKeystroke() {
-		
+
 	}
 
 	@Override
 	public void run() {
 		// while the program is tracking, check for mouse activity
-		while(ProgramTimer.trackIfTrue) {
+		while (ProgramTimer.trackIfTrue) {
 			checkMouse();
-			
-            try {
+
+			try {
 				Thread.sleep(10000);
-	            if (this.checkMouse()) {
-	            	ProgramTimer.stop();
-	            	Main.setStopLabel();
-	            }
+				if (this.checkMouse()) {
+					ProgramTimer.stop();
+					Main.setStopLabel();
+				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 		}
-		
-		
+
 	}
 
 }
--- src/core/Main.java
+++ src/core/Main.java
@@ -32,152 +32,150 @@ import javax.swing.JFormattedTextField;
 
 public class Main {
 
-    public static final int FRAME_SIZE = 300;
-    public JFrame frame;
-    public static Main gui;
-    public static JLabel lblCurrentlyNotTracking = new JLabel("Currently not tracking");
+	public static final int FRAME_SIZE = 300;
+	public JFrame frame;
+	public static Main gui;
+	public static JLabel lblCurrentlyNotTracking = new JLabel("Currently not tracking");
 
-    /**
-     * Main method that builds the GUI.
-     */
-    public static void main(String[] args) {
-        gui = new Main();
-    }
+	/**
+	 * Main method that builds the GUI.
+	 */
+	public static void main(String[] args) {
+		gui = new Main();
+	}
 
-    /**
-     * Constructor that builds the frame.
-     */
-    public Main() {
-        frame = new JFrame();
-        frame.setLocation(100, 100);
-        frame.setSize(533, 381);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("ProductivityPlus");
-        initializeComponents();
-        frame.setResizable(false);
+	/**
+	 * Constructor that builds the frame.
+	 */
+	public Main() {
+		frame = new JFrame();
+		frame.setLocation(100, 100);
+		frame.setSize(533, 381);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("ProductivityPlus");
+		initializeComponents();
+		frame.setResizable(false);
 
-        JMenuBar menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar);
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
 
-        JMenu mnFile = new JMenu("File");
-        menuBar.add(mnFile);
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
 
-        JMenuItem mntmItem = new JMenuItem("Item 1");
-        mnFile.add(mntmItem);
+		JMenuItem mntmItem = new JMenuItem("Item 1");
+		mnFile.add(mntmItem);
 
-        JMenuItem mntmItem_1 = new JMenuItem("Item 2");
-        mnFile.add(mntmItem_1);
+		JMenuItem mntmItem_1 = new JMenuItem("Item 2");
+		mnFile.add(mntmItem_1);
 
-        JMenu mnEdit = new JMenu("Edit");
-        menuBar.add(mnEdit);
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
 
-        JMenuItem mntmItem_2 = new JMenuItem("Item 1");
-        mnEdit.add(mntmItem_2);
+		JMenuItem mntmItem_2 = new JMenuItem("Item 1");
+		mnEdit.add(mntmItem_2);
 
-        JMenu mnHelp = new JMenu("Help");
-        menuBar.add(mnHelp);
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
 
-        JMenuItem mntmItem_3 = new JMenuItem("Item 1");
-        mnHelp.add(mntmItem_3);
-        frame.getContentPane()
-            .setLayout(new MigLayout("", "[531.00px]", "[337.00px]"));
+		JMenuItem mntmItem_3 = new JMenuItem("Item 1");
+		mnHelp.add(mntmItem_3);
+		frame.getContentPane().setLayout(new MigLayout("", "[531.00px]", "[337.00px]"));
 
-        JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-        frame.getContentPane().add(tabbedPane, "cell 0 0,growx,aligny top");
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, "cell 0 0,growx,aligny top");
 
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.WHITE);
-        panel.setToolTipText("");
-        tabbedPane.addTab("Program Timer", null, panel, null);
-        panel.setLayout(
-            new MigLayout("", "[121.00,grow][224.00,grow]", "[35.00][200.00,grow]"));
-        
-        JLabel lblTimerControls = new JLabel("Timer Controls");
-        lblTimerControls.setFont(new Font("Verdana", Font.BOLD, 12));
-        panel.add(lblTimerControls, "cell 0 0");
-        
-        //JLabel lblCurrentlyNotTracking = new JLabel("Currently not tracking");
-        lblCurrentlyNotTracking.setFont(new Font("Verdana", Font.BOLD, 12));
-        panel.add(lblCurrentlyNotTracking, "cell 1 0");
-                                                
-                                                JPanel panel_3 = new JPanel();
-                                                panel_3.setBackground(Color.WHITE);
-                                                panel.add(panel_3, "cell 0 1,grow");
-                                                panel_3.setLayout(new MigLayout("", "[100]", "[25][][][]"));
-                                                
-                                                        JButton btnNewButton = new JButton("Start Timer");
-                                                        panel_3.add(btnNewButton, "cell 0 0");
-                                                        btnNewButton.addMouseListener(new MouseAdapter() {
-                                                            @Override
-                                                            public void mouseClicked(MouseEvent arg0) {
-                                                                ProgramTimer programTimer = new ProgramTimer();
-                                                                Thread t = new Thread(programTimer);
-                                                                t.start();
-                                                                lblCurrentlyNotTracking.setText("Currently tracking...");
-                                                            }
-                                                        });
-                                                        btnNewButton.setFont(new Font("Verdana", Font.PLAIN, 11));
-                                                        
-                                                                JButton btnNewButton_1 = new JButton("Stop Timer");
-                                                                panel_3.add(btnNewButton_1, "cell 0 1");
-                                                                btnNewButton_1.addMouseListener(new MouseAdapter() {
-                                                                    @Override
-                                                                    public void mouseClicked(MouseEvent arg0) {
-                                                                        ProgramTimer.stop();
-                                                                        setStopLabel();
-                                                                        //lblCurrentlyNotTracking.setText("Currently not tracking.");
-                                                                    }
-                                                                });
-                                                                btnNewButton_1.setFont(new Font("Verdana", Font.PLAIN, 11));
-                                                                
-                                                                        JButton btnNewButton_2 = new JButton("Open Output");
-                                                                        panel_3.add(btnNewButton_2, "cell 0 2");
-                                                                        btnNewButton_2.setFont(new Font("Verdana", Font.PLAIN, 11));
-                                                                        
-                                                                                JButton btnNewButton_3 = new JButton("Explore Data");
-                                                                                panel_3.add(btnNewButton_3, "cell 0 3");
-                                                                                btnNewButton_3.setFont(new Font("Verdana", Font.PLAIN, 11));
-                                                                                
-                                                                                JScrollPane scrollPane = new JScrollPane();
-                                                                                panel.add(scrollPane, "cell 1 1,grow");
-                                                                                
-                                                                                JPanel panel_4 = new JPanel();
-                                                                                panel_4.setBackground(Color.WHITE);
-                                                                                scrollPane.setViewportView(panel_4);
-                                                                                panel_4.setLayout(new MigLayout("", "[258.00px]", "[300.00px]"));
-                                                                                
-                                                                                JTextArea txtrSampleData = new JTextArea();
-                                                                                txtrSampleData.setEditable(false);
-                                                                                txtrSampleData.setRows(20);
-                                                                                txtrSampleData.setText("sample data");
-                                                                                panel_4.add(txtrSampleData, "cell 0 0,growx,aligny top");
-                        btnNewButton.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent arg0) {
-                            }
-                        });
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setToolTipText("");
+		tabbedPane.addTab("Program Timer", null, panel, null);
+		panel.setLayout(new MigLayout("", "[121.00,grow][224.00,grow]", "[35.00][200.00,grow]"));
 
-        JPanel panel_1 = new JPanel();
-        panel_1.setBackground(Color.PINK);
-        tabbedPane.addTab("Pomodoro Timer", null, panel_1, null);
+		JLabel lblTimerControls = new JLabel("Timer Controls");
+		lblTimerControls.setFont(new Font("Verdana", Font.BOLD, 12));
+		panel.add(lblTimerControls, "cell 0 0");
 
-        JPanel panel_2 = new JPanel();
-        panel_2.setToolTipText("");
-        panel_2.setBackground(Color.GRAY);
-        tabbedPane.addTab("Break Stopper", null, panel_2, null);
-        panel_2.setLayout(new MigLayout("", "[]", "[343.00]"));
-        frame.setVisible(true);
+		// JLabel lblCurrentlyNotTracking = new JLabel("Currently not tracking");
+		lblCurrentlyNotTracking.setFont(new Font("Verdana", Font.BOLD, 12));
+		panel.add(lblCurrentlyNotTracking, "cell 1 0");
 
-    }
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.WHITE);
+		panel.add(panel_3, "cell 0 1,grow");
+		panel_3.setLayout(new MigLayout("", "[100]", "[25][][][]"));
 
-    /**
-     * Method to initialize all the components onto the frame.
-     */
-    public void initializeComponents() {
-    }
-    
-    public static void setStopLabel() {
-    	lblCurrentlyNotTracking.setText("Currently not tracking.");
-    }
+		JButton btnNewButton = new JButton("Start Timer");
+		panel_3.add(btnNewButton, "cell 0 0");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				ProgramTimer programTimer = new ProgramTimer();
+				Thread t = new Thread(programTimer);
+				t.start();
+				lblCurrentlyNotTracking.setText("Currently tracking...");
+			}
+		});
+		btnNewButton.setFont(new Font("Verdana", Font.PLAIN, 11));
+
+		JButton btnNewButton_1 = new JButton("Stop Timer");
+		panel_3.add(btnNewButton_1, "cell 0 1");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				ProgramTimer.stop();
+				setStopLabel();
+				// lblCurrentlyNotTracking.setText("Currently not tracking.");
+			}
+		});
+		btnNewButton_1.setFont(new Font("Verdana", Font.PLAIN, 11));
+
+		JButton btnNewButton_2 = new JButton("Open Output");
+		panel_3.add(btnNewButton_2, "cell 0 2");
+		btnNewButton_2.setFont(new Font("Verdana", Font.PLAIN, 11));
+
+		JButton btnNewButton_3 = new JButton("Explore Data");
+		panel_3.add(btnNewButton_3, "cell 0 3");
+		btnNewButton_3.setFont(new Font("Verdana", Font.PLAIN, 11));
+
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane, "cell 1 1,grow");
+
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(Color.WHITE);
+		scrollPane.setViewportView(panel_4);
+		panel_4.setLayout(new MigLayout("", "[258.00px]", "[300.00px]"));
+
+		JTextArea txtrSampleData = new JTextArea();
+		txtrSampleData.setEditable(false);
+		txtrSampleData.setRows(20);
+		txtrSampleData.setText("sample data");
+		panel_4.add(txtrSampleData, "cell 0 0,growx,aligny top");
+		btnNewButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.PINK);
+		tabbedPane.addTab("Pomodoro Timer", null, panel_1, null);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setToolTipText("");
+		panel_2.setBackground(Color.GRAY);
+		tabbedPane.addTab("Break Stopper", null, panel_2, null);
+		panel_2.setLayout(new MigLayout("", "[]", "[343.00]"));
+		frame.setVisible(true);
+
+	}
+
+	/**
+	 * Method to initialize all the components onto the frame.
+	 */
+	public void initializeComponents() {
+	}
+
+	public static void setStopLabel() {
+		lblCurrentlyNotTracking.setText("Currently not tracking.");
+	}
 
 }
