f04bb2a665c6bc45ea31fb650be78902fd68d493
darrylayers <ayersda@appstate.edu>
2018-09-29 00:09:40 -0400

    added SingletonTimer documentation

--- src/core/SingletonTimer.java
+++ src/core/SingletonTimer.java
@@ -1,32 +1,57 @@
 package core;
 
+/**
+ * SingletonTimer.java creates a single instance of the program timer. This is
+ * important because it insures only one tracker can be created at a time.
+ * 
+ * @author Austin Ayers
+ * @version 9/29/18
+ */
 public class SingletonTimer {
-	private static SingletonTimer timer;
-	private static boolean beenCalled = false;
-	
-	private SingletonTimer() {
-		
-	}
-	
-    public static SingletonTimer getInstance() {
-    	if(timer == null) {
-    		timer = new SingletonTimer();
-    	}
-    	return timer;
+
+    private static SingletonTimer timer;
+    private static boolean beenCalled = false;
+
+    /**
+     * Private constructor used to make this class a singleton.
+     */
+    private SingletonTimer() {
+
     }
-    
+
+    /**
+     * Getter that returns a single instance from the SingletonTimer's private
+     * constructor.
+     * 
+     * @return returns a singleton SingletonTimer.
+     */
+    public static SingletonTimer getInstance() {
+        if (timer == null) {
+            timer = new SingletonTimer();
+        }
+        return timer;
+    }
+
+    /**
+     * This method creates a new program timer object if the boolean field
+     * beenCalled is false, meaning there has not yet been a program timer
+     * created.
+     */
     public void callTimer() {
-    	if (beenCalled == false) {
-        	beenCalled = true;
+        if (beenCalled == false) {
+            beenCalled = true;
             ProgramTimer programTimer = new ProgramTimer();
             Thread t = new Thread(programTimer);
             t.start();
-    	}
+        }
 
     }
-    
+
+    /*
+     * This method allows the callTimer() method
+     * to be able to create a new program timer object.
+     */
     public static void setBeenCalled() {
-    	beenCalled = false;
+        beenCalled = false;
     }
-
 }
